94f9477af152071c03141417dce55b5f4b850812
minimous <khanhhoai95@gmail.com>
2016-08-23 22:37:57 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
