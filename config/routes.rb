30878fcb7fe5168d262f6c5ed8e7731c0f737748
minimous <khanhhoai95@gmail.com>
2016-08-29 18:13:49 +0700

    Create show method

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   root 'static_pages#home'
   # root 'application#hello'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
