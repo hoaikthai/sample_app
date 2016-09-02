9c0e79c4dec50146c4c298a023b707c39681c615
minimous <khanhhoai95@gmail.com>
2016-09-02 23:59:03 +0700

    Create Sessions test

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   root 'static_pages#home'
   # root 'application#hello'
   resources :users
