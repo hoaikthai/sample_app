7994139a99718d854257a61656fcb6286ee642d7
minimous <khanhhoai95@gmail.com>
2016-09-02 10:24:54 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
+  post '/signup', to: 'users#create'
   root 'static_pages#home'
   # root 'application#hello'
   resources :users
